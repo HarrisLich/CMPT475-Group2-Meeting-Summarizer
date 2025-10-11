0b431cc4d725d208013bcdf5ef1f32748109edb1
Marko <markopavic7777@gmail.com>
2025-10-10 20:12:22 -0400

    supabase client in the route handlers

---
+++ server/database/middleware.py
@@ -0,0 +1,7 @@
+from fastapi import Request, Depends
+from auth.dependencies import get_current_user
+from .supabase_service import get_supabase
+   
+   async def get_db_with_auth(current_user = Depends(get_current_user)):
+       supabase = get_supabase()
+       return supabase.set_auth_override(current_user["uid"])
